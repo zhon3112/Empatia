5ba958c326887e0934d94d9f383aae1eda18037f
zhon <zhon12shou31@gmail.com>
2024-08-14 21:37:38 +0900

    ‘ビューファイルとルーティング’

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
 
+  get "sign_up", to: "users#new"
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
