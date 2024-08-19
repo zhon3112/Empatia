0336318ceed7a7f0edba936c844945de6bb3aad4
zhon <zhon12shou31@gmail.com>
2024-08-19 21:33:36 +0900

    ‘Mypageのコンフリ回避用’

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
 
-  enum status: { public: 0, private: 1 }
+  enum status: { visible: 0, is_public: 1 }
 
   def make_private
-    update(status: :private)
+    update(status: :is_public)
   end
 end
