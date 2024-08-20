6243c2a5d534ac194fb97982baa4d8e754d086d2
zhon <zhon12shou31@gmail.com>
2024-08-20 21:43:31 +0900

    ‘ログアウトとユーザー登録後の遷移’

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_08_15_103159) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -31,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_15_103159) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
