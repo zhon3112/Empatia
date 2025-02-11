7c247d8bff2849be857ddd6755aea6b0df2992af
zhon <zhon12shou31@gmail.com>
2025-02-11 21:07:49 +0900

    ‘N+1問題1’

--- app/controllers/my_posts_controller.rb
+++ app/controllers/my_posts_controller.rb
@@ -1,7 +1,8 @@
 class MyPostsController < ApplicationController
   def index
     @post = Post.new
-    @posts = Post.where(user_id: current_user.id)
+    @posts = current_user.posts.eager_load(:likes)
+    @likes_by_post = Like.where(post_id: @posts.map(&:id)).group_by(&:post_id)
     @message = @posts.empty? ? "あなたの投稿はありません" : nil
   end
 end
