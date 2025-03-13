293ec58f60bb7a7b77baeb1bfdb584ff4f6a7cec
zhon <zhon12shou31@gmail.com>
2025-03-13 22:13:31 +0900

    ‘トップページと説明カード’

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,13 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['M PLUS Rounded 1c', 'ui-sans-serif', 'system-ui'],
+        serif: ['Merriweather', 'ui-serif', 'Georgia'],
+      },
+    },
+  },
   plugins: [require("daisyui")],
 }
