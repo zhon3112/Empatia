855930e9579ebe078f4959c5a378e6a8ad9f702a
zhon <zhon12shou31@gmail.com>
2025-03-20 20:06:01 +0900

    ‘投稿デザイン2’

--- app/controllers/my_posts_controller.rb
+++ app/controllers/my_posts_controller.rb
@@ -3,7 +3,7 @@ class MyPostsController < ApplicationController
 
   def index
     @post = Post.new
-    @posts = current_user.posts.eager_load(:likes)
+    @posts = current_user.posts.eager_load(:likes).order(created_at: :desc)
     @likes_by_post = Like.where(post_id: @posts.map(&:id)).group_by(&:post_id)
     @message = @posts.empty? ? "あなたの投稿はありません" : nil
   end
